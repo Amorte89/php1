22bb2c9dee20b73a3a5afb471d3b3d22764b467b
Amorte89 <nikitakadilnikov89@gmail.com>
2017-07-26 21:01:17 +0300

    Add files via upload

--- date.php
+++ date.php
@@ -14,10 +14,31 @@ $b = 2;
 <body>
 <h1 style="text-align: center">
     <? echo $h1;
+$i = 0;
+$array_menu = [];
+
+$array_menu[$i]['url']='#';
+$array_menu[$i++]['name']='Home';
+
+$array_menu[$i]['url']='#';
+$array_menu[$i++]['name']='section 1';
+
+$array_menu[$i]['url']='#';
+$array_menu[$i++]['name']='section 2';
+
+echo "<ul><br>";
+for ($i=0;$i<count($array_menu);$i++)
+{
+    echo ($_SERVER["REQUEST_URI"] == $array_menu[$i]['url']) ?: '<li>';
+    echo "<a href=\"".$array_menu[$i]['url']."\">".$array_menu[$i]['name']."</a></li>\n";
+}
+echo "</ul>";
     ?>
 <footer>
     <?php
-    echo  date("Y");
+    echo  date("Y")."<br>";
+
+    echo date("G:i s", time());
     ?>
 </footer>
 </h1>
